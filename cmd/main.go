d5969c243769f592ef21a6aa7fbe1d03924dc495
Rana Tarek Hassan <rana.hassan@weave.works>
2023-09-27 17:58:57 +0300

    Create api
    kubebuilder create api --group clusters --version v1alpha1 --kind AutomatedClusterDiscovery

--- cmd/main.go
+++ cmd/main.go
@@ -31,6 +31,9 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	clustersv1alpha1 "weave.works/cluster-reflector-controller/api/v1alpha1"
+	"weave.works/cluster-reflector-controller/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -42,6 +45,7 @@ var (
 func init() {
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
 
+	utilruntime.Must(clustersv1alpha1.AddToScheme(scheme))
 	//+kubebuilder:scaffold:scheme
 }
 
@@ -85,6 +89,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.AutomatedClusterDiscoveryReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "AutomatedClusterDiscovery")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
