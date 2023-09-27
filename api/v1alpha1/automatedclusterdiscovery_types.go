f5317d732f8a04d4eaaf781817c10e249d986ee5
Rana Tarek Hassan <rana.hassan@weave.works>
2023-09-27 18:01:57 +0300

    Add types and generate crd

--- api/v1alpha1/automatedclusterdiscovery_types.go
+++ api/v1alpha1/automatedclusterdiscovery_types.go
@@ -28,8 +28,11 @@ type AutomatedClusterDiscoverySpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of AutomatedClusterDiscovery. Edit automatedclusterdiscovery_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// Name is the name of the cluster
+	Name string `json:"foo,omitempty"`
+
+	// Type is the provider type
+	Type string `json:"type,omitempty"`
 }
 
 // AutomatedClusterDiscoveryStatus defines the observed state of AutomatedClusterDiscovery
