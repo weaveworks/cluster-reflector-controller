b2fa8e8482b951c05579b0827077fb89c27e0efd
Kevin McDermott <kevin@weave.works>
2023-10-18 09:29:19 +0100

    Add documentation for the provider functionality.

--- pkg/providers/provider.go
+++ pkg/providers/provider.go
@@ -4,13 +4,31 @@ import (
 	"context"
 
 	"k8s.io/client-go/tools/clientcmd/api"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
+// Provider implementations query the API for cluster services e.g. EKS and AKS.
 type Provider interface {
+	// ListClusters queries the cluster service for a list of clusters.
 	ListClusters(ctx context.Context) ([]*ProviderCluster, error)
+
+	// ClusterID implementations are responsible for generating a unique ID from
+	// a cluster.
+	// How this Unique ID is calculated varies across providers.
+	// But it must be able to be matched to the UniqueID on a ProviderCluster.
+	ClusterID(ctx context.Context, kubeClient client.Reader) (string, error)
 }
 
+// ProviderCluster is a representation of the cluster from the cluster service
+// that can be used to create a GitopsCluster.
 type ProviderCluster struct {
-	Name       string
+	// This is the name of the cluster as provided by the cluster service.
+	Name string
+	// This is the unique ID for this cluster.
+	UniqueID string
+
+	// This is the KubeConfig from the cluster service used to connect to it.
+	// This is likely to be a high privilege token.
+	// The token MUST NOT require the execution of a binary (ExecConfig).
 	KubeConfig *api.Config
 }
